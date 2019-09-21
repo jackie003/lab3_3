42a55400476caff9dff9e536c630afab12ea09dc
jackie003 <6035512003@psu.ac.th>
2019-09-22 02:04:53 +0700

    com1

---
+++ src/CharacterCard.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+
+
+class CharacterCard extends React.Component {
+    state = {
+        active: false
+    }
+    
+       
+    activate = () => {
+        this.setState({
+            active:true
+        });
+        if (this.state.active === false)
+        this.props.activationHandler(this.props.value);
+
+    }
+    componentDidUpdate = (prevProps) => {
+        console.log(this.props.attempt);
+        if (prevProps.attempt !== this.props.attempt || prevProps.restartGame !==this.props.restartGame) {
+            this.setState({ active: false })
+            console.log('...');
+        }
+    }
+    render() {
+        //  let activateClass = this.state.act
+        let className = `card ${this.state.active ? 'activeCard' : ''}`
+        return (
+            <div className={className} onClick={this.activate}>
+                {this.props.value}
+            </div>
+        )
+    }
+}
+
+
+export default CharacterCard;
